baad6aff5f6fe870255cfa228fd60c605a869d93
lizrd-queen <lizrd.roberts96@gmail.com>
2023-10-12 21:06:42 -0500

    add 3x3 grid, some flexbox styling

--- java.js
+++ java.js
@@ -1,4 +1,17 @@
-let outerContainer =getElementbyId('outerContainer')
+document.addEventListener("DOMContentLoaded", function () {
+    var outerContainer = document.getElementById('outerContainer');
+    
+    var numRows = 3;
+    var numCols = 3;
 
-
-var gridPiece = document.createElement("div");
+    for (var i = 0; i < numRows; i++) {
+        var row = document.createElement("div");
+        row.classList.add("grid-row");
+        for (var j = 0; j < numCols; j++) {
+            var div = document.createElement("div");
+            div.classList.add("grid-item");
+            row.appendChild(div);
+        }
+        outerContainer.appendChild(row);
+    }
+});
